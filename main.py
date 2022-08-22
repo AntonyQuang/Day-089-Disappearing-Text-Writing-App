63780e6386ba21c4abe3a3e923e6ee745ec913dc
Antony Quang <antony.quang@googlemail.com>
2022-08-22 21:29:28 +0100

    Add formatting

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ from tkinter import ttk
 
 root = Tk()
 root.title("Keep typing and nobody gets hurt!")
-root.geometry("800x800+0+0")
+root.geometry("644x740+0+0")
+root.configure(background="#C3B5CF")
 
-frame = ttk.Frame(root, padding=10)
+frame = ttk.Frame(root, padding=10, style="custom.TFrame")
 frame.grid()
+
+# Tk Styles
+style = ttk.Style(root)
+style.configure('.', background='#C3B5CF')
+style.configure('title.TLabel', font=('Calibri', 30, "bold"), foreground="#5F3027")
+style.configure('text.TLabel', font=('Calibri', 15), foreground="#5F3027")
+style.configure('custom.TButton', font=('Calibri', 11), foreground="#5F3027", bg="#88768C")
+style.configure('timer.TEntry', font=('Courier', "52"))
 
 # Variables
 
@@ -58,30 +67,34 @@ def reset():
     timer.delete(0, END)
     timer.insert(0, str(time))
     text_box.configure(state="normal")
+    text_box.delete("1.0", "end")
     text_box.focus()
 
 
 # Widgets
-title = ttk.Label(frame, text="Keep typing and nobody gets hurt!")
-title.grid(row=0, column=1)
+title = ttk.Label(frame, text="Keep Typing and Nobody Gets Hurt!", style="title.TLabel", justify="center")
+title.grid(row=0, column=0, columnspan=2, pady=10)
 
 explanation = ttk.Label(frame, text="Once you start typing, you have only 5 seconds to keep typing. Fail to keep "
-                                    "typing, and the whole text will disappear!")
-explanation.grid(row=1, column=1)
+                                    "typing, and the whole text will disappear!", style="text.TLabel", wraplength=600,
+                        justify="center")
+explanation.grid(row=1, column=0, columnspan=2)
 
-timer = ttk.Entry(frame)
+timer = ttk.Entry(frame, justify="center")
+timer.configure(font=('Calibri', "30"), width=3)
 timer.insert(0, str(time_limit))
-timer.grid(row=2, column=1)
+timer.grid(row=2, column=0, columnspan=2, pady=10)
 
-text_box = scrolledtext.ScrolledText(frame, height=40)
-text_box.grid(row=3, column=1, columnspan=3)
+text_box = scrolledtext.ScrolledText(frame, height=20, width=60)
+text_box.grid(row=3, column=0, columnspan=2, pady=10)
+text_box.configure(font=('Calibri', "14"), wrap=WORD)
 text_box.focus()
 
-quit_button = ttk.Button(frame, text="Quit", command=root.destroy)
-quit_button.grid(row=4, column=1, sticky="W")
+quit_button = ttk.Button(frame, text="Quit", command=root.destroy, style="custom.TButton")
+quit_button.grid(row=4, column=0, sticky="W")
 
-reset_button = ttk.Button(frame, text="Reset", command=reset)
-reset_button.grid(row=4, column=2)
+reset_button = ttk.Button(frame, text="Reset", command=reset, style="custom.TButton")
+reset_button.grid(row=4, column=1, sticky="E")
 
 text_box.bind("<KeyPress>", typing)
 
